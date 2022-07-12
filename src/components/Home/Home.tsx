6bc1fd75bf2ab8bab0786f6c2ce7cde0f9ef1de0
msaintil-afk <marcus.saintil@gmail.com>
2022-07-11 23:59:36 -0500

    first commit

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+import car_inventory_image from '../../assets/images/car_inventory_image.jpg';
+import { Link } from 'react-router-dom';
+
+interface Props{
+    title: string;
+}
+
+
+// Create Styled Components with styled-components
+const Root = styled("div")({
+    padding: 0,
+    margin: 0
+})
+const NavbarContainer = styled('div')( {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+})
+const Logo = styled('h1')({
+    margin: '0 0 0 0.45em'
+})
+const LogoA = styled('a')( {
+    color: 'rgb(28,24,22)',
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none'
+})
+const LogoNavigation = styled('ul')( {
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    display: 'flex'
+})
+
+const NavA = styled(Link)({
+    display: 'block',
+    padding: '1em',
+    color: 'black'
+})
+const Main = styled('main')( {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${car_inventory_image});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute',
+})
+const MainText = styled('div')({
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+})
+
+export const Home = ( props:Props) => {
+ 
+    return (
+        <Root>
+            <NavbarContainer>
+                <Logo>
+                    <LogoA href="#">4x4 Cars</LogoA>
+                </Logo>
+                <LogoNavigation>
+                    <li>
+                        <NavA to='/'>Home</NavA>
+                    </li>
+                    <li>
+                        <NavA to='/'>Dashboard</NavA>
+                    </li>
+                    <li>
+                        <NavA to='/'>SignIn</NavA>
+                    </li>
+                </LogoNavigation>
+            </NavbarContainer>
+            <Main>
+                <MainText>
+                    <h1>{props.title}</h1>
+                    <p> of rugged cars</p>
+                    <Button color='primary' variant='contained' component={Link} to='/dashboard'>See the Car collection</Button>
+                </MainText>
+            </Main>
+
+        </Root>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Home,Dashboard,SignIn } from './components';
+import './styles.css'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home title  ={'Rugged Inventory'}/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signin' element={<SignIn />} />
+        </Routes>
+      </Router>
+
+    
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
